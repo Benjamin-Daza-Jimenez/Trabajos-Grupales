6927326938bf7f8553a9e4c998eca71e785f951e
benja nicolas <benja.daza.jimenez@gmail.com>
2024-08-25 17:56:05 -0400

    Trabajo terminado

--- Lab1 SO/Lab1.cpp
+++ Lab1 SO/Lab1.cpp
@@ -5,73 +5,60 @@
 #include <vector>
 #include <cstdio>
 #include <cstdlib>
+#include <algorithm>
 using namespace std;
+
+bool existeVector(vector<string> lista, string word){
+    return find(lista.begin(),lista.end(),word) != lista.end();
+    }
+
 
 int main(){
     string line, path="./archivos_deportes";
     DIR *direccion;
-    vector<string> deporte;
-    vector<string> categoria;
+    vector<string> deporte_categoria;
     direccion=opendir(path.c_str());
+    mkdir("./Carpeta actual");
     while(dirent*i=readdir(direccion)){   
         string name = i->d_name;
         if(name!="." && name!=".."){
-            string dep_aux,cat_aux, path_archivo="./archivos_deportes/"+name;                      //Recorre el directorio
+            string dep_aux,cat_aux, medalla,path_archivo="./archivos_deportes/"+name;                      //Recorre el directorio
             ifstream archivo(path_archivo);
-            archivo.is_open();                          //Leer cada archivo
+            archivo.is_open();                  
             int j=0;
-            while(getline(archivo, line)){                      //Obtener las 3 lineas del archivo
-                bool flag = false;
-                if(j==0){//Deporte
-                    dep_aux=line;
-                    for(int k=0;k<deporte.size();k++){
-                        if(deporte[k] == line){
-                            flag = true;
-                        }
-                    }
-                    if(flag==false){//Crear directorio
-                        deporte.push_back(line);
-                        string p="./"+line;
-                        mkdir(p.c_str());
-                    }
-                }
-                else if(j==1){//Categoría
-                    cat_aux=line;
-                    for(int k=0; k<categoria.size();k++){
-                        if(categoria[k] == line){
-                            flag = true;
-                        }
-                    }
-                    if(flag==false){//Crear directorio y los dos directorios de medalla o no
-                        categoria.push_back(line);
-                        string p="./"+dep_aux+"/"+line;
-                        mkdir(p.c_str());
-                        p="./"+dep_aux+"/"+line+"/ConMedalla";
-                        mkdir(p.c_str());
-                        p="./"+dep_aux+"/"+line+"/SinMedalla";
-                        mkdir(p.c_str());
-                    }
-                }
-                else if(j==2){//Medalla
-                    if(line=="Sin Medalla"){
-                        string p="./"+dep_aux+"/"+cat_aux+"/SinMedalla/"+name;
-                        //archivo guardarlo en....
-                    }
-                    else{
-                        string p="./"+dep_aux+"/"+cat_aux+"/ConMedalla/"+name;
-                        //system(("move "+path_archivo+" "+p+"/"+name).c_str());
-                        cout<<p<<endl;
-                        if(rename(path_archivo.c_str(),p.c_str())!=0){
-                            cout<<"Todo mal"<<endl;
-                        };
-                    }
-                }
+            while(getline(archivo, line)){//Obtener las 3 lineas del archivo
+                if(j==0){dep_aux=line;}
+                else if(j==1){cat_aux=line;}
+                else if(j==3){medalla=line;}
                 j++;
             }
-            archivo.close();
+            string aux=dep_aux+"/"+cat_aux;
+            if(existeVector(deporte_categoria,aux)==false){
+                deporte_categoria.push_back(aux);
+                if(existeVector(deporte_categoria,dep_aux)==false){
+                    deporte_categoria.push_back(dep_aux);
+                    mkdir(("./Carpeta actual/"+dep_aux).c_str());
+                }
+                mkdir(("./Carpeta actual/"+aux).c_str());
+                mkdir(("./Carpeta actual/"+aux+"/ConMedalla").c_str());
+                mkdir(("./Carpeta actual/"+aux+"/SinMedalla").c_str());
+            }
+            if(medalla=="SinMedalla"){
+                string p="Carpeta actual/"+dep_aux+"/"+cat_aux+"/SinMedalla/"+name;
+                archivo.close();
+                if(rename(path_archivo.c_str(),p.c_str())!=0){
+                    cout<<"Todo mal"<<endl;
+                };
+            }
+            else{
+                string p="Carpeta actual/"+dep_aux+"/"+cat_aux+"/ConMedalla/"+name;                        //system(("move "+path_archivo+" "+p+"/"+name).c_str());
+                archivo.close();
+                if(rename(path_archivo.c_str(),p.c_str())!=0){
+                    cout<<"Todo mal"<<endl;
+                }
+            }
         }
     }
     closedir(direccion);
-    system("pause");
     return 0;
 }
